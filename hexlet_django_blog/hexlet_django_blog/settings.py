fb29c19125267c5646e5b88b040ef5b7cc9545d4
Viktor <108342666+jjoskeyman@users.noreply.github.com>
2023-08-21 16:54:05 +0300

    some update

--- hexlet_django_blog/hexlet_django_blog/settings.py
+++ hexlet_django_blog/hexlet_django_blog/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hexlet_django_blog',
-    'women.apps.WomenConfig'
+    'women.apps.WomenConfig',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
