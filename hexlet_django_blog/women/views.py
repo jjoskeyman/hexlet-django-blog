9babfbd0d8a2339fe43ba18748676745225af8e1
Viktor <108342666+jjoskeyman@users.noreply.github.com>
2023-08-25 17:35:12 +0300

    registration*authentication

--- hexlet_django_blog/women/views.py
+++ hexlet_django_blog/women/views.py
@@ -1,10 +1,11 @@
+from django.contrib.auth import logout, login
+from django.contrib.auth.views import LoginView
 from django.core.paginator import Paginator
 from django.http import HttpResponseNotFound, Http404, HttpResponse
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, DeleteView
 from .forms import *
-from .models import *
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .utils import *
@@ -113,8 +114,38 @@ def contact(request):
     return render(request, 'women/contact.html', {'page_obj': page_obj, 'title': 'Обратная связь', 'menu': menu})
 
 
-def login(request):
-    return HttpResponse("Авторизация")
+class RegisterUser(DataMixin, CreateView):
+    form_class = RegisterUserForm
+    template_name = 'women/register.html'
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Регистрация")
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('home')
+
+
+class LoginUser(DataMixin, LoginView):
+    form_class = LoginUserForm
+    template_name = 'women/login.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Авторизация")
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
 
 
 def page_not_found(request, exception):
